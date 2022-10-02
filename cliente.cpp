12f2ee11d75c1748b46ae004854995a3c1e3ad94
tomashm01 <tomashidalgomartin@gmail.com>
2022-10-02 13:47:44 +0200

    feat:clases y validacion user

--- cliente.cpp
+++ cliente.cpp
@@ -1,84 +1,19 @@
-#include <cstdio>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-
+#include "models/Client.cpp"
 using namespace std;
 
-int main(int argc, char **argv)
+int main ( )
 {
-    string usuario, password;
-    int c;
-
-    if (argc < 3)
-    {
-        cout << "Error: No se ha introducido los datos de entrada correctos" << endl;
-        return -1;
-    }
-
-    while ((c = getopt(argc, argv, "u:p:")) != -1)
-    {
-        switch (c)
-        {
-        case 'u':
-            usuario = optarg;
-            break;
-        case 'p':
-            password = optarg;
-            break;
-        case '?':
-            cout << "Error: Opcion invalida" << endl;
-            break;
-        default:
-            cout << "Opcion no valida" << endl;
-            break;
-        }
-    }
-
-    // Enviar usuario y contraseña al servidor por TCP (socket)
-    int sd;
-    struct sockaddr_in sockname;
-    char buffer[100];
-    socklen_t len_sockname;
-
-    sockname.sin_family = AF_INET;
-    sockname.sin_port = htons(2000);
-    sockname.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-    // Enviar usuario y contraseña al servidor
-    if (connect(sd, (struct sockaddr *)&sockname, sizeof(sockname)) == -1)
-    {
-        perror("Error en la conexion\n");
-        exit(1);
-    }
-
-    // Enviar usuario al servidor
-    string data = usuario + ";" + password;
-    strcpy(buffer, data.c_str());
-    if (send(sd, buffer, strlen(buffer), 0) == -1)
-    {
-        perror("Error en el envio\n");
-        exit(1);
-    }
-
-    // Recibir respuesta del servidor
-    if (recv(sd, buffer, sizeof(buffer), 0) == -1)
-    {
-        perror("Error en la recepcion\n");
-        exit(1);
-    }
-    cout << buffer << endl;
-
-    close(sd);
-
-    return 0;
+    Client client;
+	client.connectToServer();
+	do
+	{
+		client.freeBuffer();
+		cout<<"Enviar mensaje: ";
+		char msg[100];
+		cin.getline(msg, 100);
+		client.sendMsg(msg);
+	}while(strcmp(client.getBuffer(), "0") != 0);
+	client.closeConnection();
+	return 0;
 }
+		
--- server.cpp
+++ server.cpp
@@ -1,110 +1,70 @@
-#include <cstdio>
-#include <stdlib.h>
-#include <fstream>
-#include <iostream>
-#include <string>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-
+#include "models/Server.cpp"
+#include <signal.h>
 using namespace std;
 
-bool validateUser(string file_name, string user, string password)
+#define MAX_CLIENTS 30
+#define PORT 2063
+#define MAX_GAMES 10
+#define FILE "users.txt"
+
+bool loginUser(string username, string password)
 {
-    string line;
-    // Pasa a char* el nombre del archivo
-    ifstream myfile(file_name.c_str());
-    if (myfile.is_open())
-    {
-        while (getline(myfile, line, ','))
-        {
-            if (line == user)
-            {
-                getline(myfile, line, ',');
-                if (password == line)
-                {
-                    cout << "Usuario encontrado:" << password << ";" << endl;
-                    return true;
-                }
-            }
-        }
-    }
-    myfile.close();
-    return false;
+	string line;
+	string data = username + "," + password;
+	ifstream myfile(FILE);
+	if (myfile.is_open())
+	{
+		while (getline(myfile, line))
+		{
+			if (strcmp(line.c_str(), data.c_str()) == 0)
+			{
+				return true;
+			}
+		}
+		myfile.close();
+	}
+	return false;
+}
+
+bool registerUser(string username, string password)
+{
+	if (!loginUser(username, password))
+	{
+		ofstream myfile;
+		myfile.open(FILE, ios::app);
+		myfile <<'\n'<< username + "," + password;
+		myfile.close();
+		return true;
+	}
+	return false;
+}
+
+void manejador (int signum){
+    printf("\nSe ha recibido la señal sigint\n");
+    signal(SIGINT,manejador);
+	exit(0);
 }
 
 int main()
 {
-    string usuario, password;
+	Server server;
+	// Maximo hay 30 clientes en cola de espera y se abre en puerto 2060
+	server.bindListen(PORT, MAX_CLIENTS);
+	signal(SIGINT,manejador);
 
-    int sd, new_sd;
-    struct sockaddr_in sockname, from;
-    char buffer[100];
-    socklen_t from_len;
+	while (true)
+	{
+		server.acceptRequest();
+		do
+		{
+			server.freeBuffer();
+			server.receiveRequest();
+			cout << "Mensaje recibido: " << server.getBuffer() << endl;
+		} while (strcmp(server.getBuffer(), "0") != 0);
+		cout << "Cerrando conexion" << endl;
+		server.closeNewSD();
+		server.closeSD();
+	}
 
-    struct hostent *host;
-
-    sd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sd == -1)
-    {
-        perror("No se puede abrir el socket cliente\n");
-        exit(1);
-    }
-
-    sockname.sin_family = AF_INET;
-    sockname.sin_port = htons(2000);
-    sockname.sin_addr.s_addr = INADDR_ANY;
-
-    if (bind(sd, (struct sockaddr *)&sockname, sizeof(sockname)) == -1)
-    {
-        perror("Error en la operación bind");
-        exit(1);
-    }
-
-    while (true)
-    {
-        memset(buffer, 0, sizeof(buffer));
-        if (listen(sd, 1) == -1)
-        {
-            perror("Error en la operación listen");
-            exit(1);
-        }
-
-        // Recibir usuario del cliente
-        from_len = sizeof(from);
-        new_sd = accept(sd, (struct sockaddr *)&from, &from_len);
-        if (new_sd == -1)
-        {
-            perror("Error en la operación accept");
-            exit(1);
-        }
-
-        recv(new_sd, buffer, 100, 0);
-
-        // Separar usuario y contraseña del buffer y pasarlo a string
-        string str(buffer);
-        usuario = str.substr(0, str.find(";"));
-        password = str.substr(str.find(";") + 1, str.length());
-
-        memset(buffer, 0, sizeof(buffer));
-
-        if (!validateUser("users.txt", usuario, password))
-        {
-            memset(buffer, 0, sizeof(buffer));
-            strcpy(buffer, "Usuario no encontrado");
-            send(new_sd, buffer, 100, 0);
-            
-        }
-        memset(buffer, 0, sizeof(buffer));
-        strcpy(buffer, "Usuario encontrado");
-        send(new_sd, buffer, 100, 0);
-        close(new_sd);
-    }
-
-    close(sd);
-    return 0;
+	return 0;
 }
